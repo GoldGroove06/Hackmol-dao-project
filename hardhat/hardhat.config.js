5befdf8eccd43b6ee721b9486976ee61be74bf7d
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-04-10 15:16:30 +0530

    initial commit for contratcs

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.20", // Adjust according to your contract
+  networks: {
+    hardhat: {
+      chainId: 31337, // Default Hardhat Network ID
+    },
+  },
+};
